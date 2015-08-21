7452fee8ce7a87b5bc7ba8b35531de4d18708e30
Henry Ambicho Trujillo <hambicho@machambicho.local>
2015-08-21 15:10:42 -0300

    Implementation of view from xib, in design and programatically

--- 001CustomView_iOS_ObjC/001CustomView_iOS_ObjC/ViewController.m
+++ 001CustomView_iOS_ObjC/001CustomView_iOS_ObjC/ViewController.m
@@ -8,6 +8,9 @@
 
 #import "ViewController.h"
 
+#import "SubClass.h"
+#import "LabelMadness.h"
+
 @interface ViewController ()
 
 @end
@@ -17,6 +20,19 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    // Add a custom view's programmatically (position using 1/2 width and height )
+    SubClass *oneView = [[SubClass alloc] init];
+    oneView.center = CGPointMake(80 + 80, 282 + 40);
+    oneView.backgroundColor = [UIColor blueColor];
+    [self.view addSubview:oneView];
+
+    
+    LabelMadness *labelMadness = [[LabelMadness alloc] init];
+    labelMadness.center = CGPointMake(60 + 100, 370 + 50);
+    labelMadness.backgroundColor = [UIColor greenColor];
+    [self.view addSubview:labelMadness];
+    
 }
 
 - (void)didReceiveMemoryWarning {
