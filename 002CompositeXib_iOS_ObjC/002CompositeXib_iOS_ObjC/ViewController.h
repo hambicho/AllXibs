5099c3cc3a0d855bc13e4e513c9ee5e025f86d9b
Henry Ambicho Trujillo <hambicho@machambicho.local>
2015-08-21 15:21:00 -0300

    Creation of Second proye in Objetive-C called CompositeXib

---
+++ 002CompositeXib_iOS_ObjC/002CompositeXib_iOS_ObjC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  002CompositeXib_iOS_ObjC
+//
+//  Created by Henry Ambicho Trujillo on 8/21/15.
+//  Copyright (c) 2015 Henry Ambicho Trujillo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
