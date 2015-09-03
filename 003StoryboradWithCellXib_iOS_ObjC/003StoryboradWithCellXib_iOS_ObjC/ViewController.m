7be90df3b9b6b971029a17a2ab978a051b02c9e3
Henry AT <henry.ambicho@mercadolibre.cl>
2015-09-03 17:05:58 -0300

    implementarion os simple table with static NSArray

--- 003StoryboradWithCellXib_iOS_ObjC/003StoryboradWithCellXib_iOS_ObjC/ViewController.m
+++ 003StoryboradWithCellXib_iOS_ObjC/003StoryboradWithCellXib_iOS_ObjC/ViewController.m
@@ -9,7 +9,7 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+@property (nonatomic, strong) NSArray *items;
 @end
 
 @implementation ViewController
@@ -17,11 +17,30 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    _items = @[@"Uno",@"Dos",@"Tres",@"Cuatro",@"Cinco",@"Seis",@"Siete"];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return [_items count];
+}
+
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    UITableViewCell *cell = [tableView dequeueReusableHeaderFooterViewWithIdentifier:@"cellIdentifier"];
+    if (!cell) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"cellIdentifier"];
+    }
+    cell.textLabel.text = [_items objectAtIndex:indexPath.row];
+    return cell;    
+}
+
+
 
 @end
